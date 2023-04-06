c5b3dc311db3c9c6421d80158920b4d6f1e4c737
root <root@ip-172-26-8-175.us-west-2.compute.internal>
2023-04-06 20:06:16 +0000

    updates the docs route

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
+const docsRoute = require('./routes/v1/docs.route');
 
 const app = express();
 
@@ -20,6 +21,8 @@ if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
+
+app.use('/v1/docs', docsRoute)
 
 // set security HTTP headers
 app.use(helmet());
